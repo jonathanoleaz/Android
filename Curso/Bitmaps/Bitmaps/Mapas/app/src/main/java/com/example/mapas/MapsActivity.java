12339324a2430554177216dcf7b522c480dc4e69
jonathanoleaz <joleaz1200@alumno.ipn.mx>
2019-04-02 12:12:49 -0600

    app de mapas

--- Curso/Bitmaps/Bitmaps/Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
+++ Curso/Bitmaps/Bitmaps/Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.mapas;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,14 +16,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+
+    private Button btnMarcador;
+    private Button btnLineas;
+    private Button btnPoligono;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnMarcador = (Button)findViewById(R.id.button1);
+        btnMarcador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertarMarcador();
+            }
+        });
+
+
+
     }
 
 
@@ -41,6 +62,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(19, -99);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker CDMX"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+    private void insertarMarcador() {
+
+        System.out.println("insertando punto");
+        LatLng sydney = new LatLng(19, -99);
+
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Pais: Australia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
