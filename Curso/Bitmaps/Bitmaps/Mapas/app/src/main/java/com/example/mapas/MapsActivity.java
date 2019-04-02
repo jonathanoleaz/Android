57d42ed43fbd1df7f34c2b6a84ec77b4e7cfb4aa
jonathanoleaz <joleaz1200@alumno.ipn.mx>
2019-04-02 13:36:54 -0600

    ya funciona bien lo del add

--- Curso/Bitmaps/Bitmaps/Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
+++ Curso/Bitmaps/Bitmaps/Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
@@ -1,39 +1,42 @@
 package com.example.mapas;
 
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
 
     private Button btnMarcador;
     private Button btnLineas;
     private Button btnPoligono;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
 
-        btnMarcador = (Button)findViewById(R.id.button1);
+        btnMarcador = (Button)findViewById(R.id.btnMarcador);
         btnMarcador.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,34 +44,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        btnLineas = (Button)findViewById(R.id.btnLineas);
+        btnLineas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarLineas();
+            }
+        });
 
-
+        btnPoligono = (Button)findViewById(R.id.btnPoligono);
+        btnPoligono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarPoligono();
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(19, -99);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker CDMX"));
+        mMap.getUiSettings().setMapToolbarEnabled(false);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(
+                        MapsActivity.this,
+                        "Marcador pulsado:\n" +
+                                marker.getTitle(),
+                        Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
     }
-
 
     private void insertarMarcador() {
-
         System.out.println("insertando punto");
         LatLng sydney = new LatLng(19, -99);
 
@@ -76,6 +88,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(sydney)
                 .title("Pais: Australia"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 
+    private void mostrarLineas()
+    {
+        //Dibujo con Lineas
+
+        PolylineOptions lineas = new PolylineOptions()
+                .add(new LatLng(45.0, -12.0))
+                .add(new LatLng(45.0, 5.0))
+                .add(new LatLng(34.5, 5.0))
+                .add(new LatLng(34.5, -12.0))
+                .add(new LatLng(45.0, -12.0));
+
+        lineas.width(8);
+        lineas.color(Color.RED);
+
+        mMap.addPolyline(lineas);
+    }
+
+    private void mostrarPoligono()
+    {
+        //Dibujo con Poligonos
+
+        PolygonOptions rectangulo = new PolygonOptions()
+                .add(new LatLng(45.0, -12.0),
+                        new LatLng(45.0, 5.0),
+                        new LatLng(34.5, 5.0),
+                        new LatLng(34.5, -12.0),
+                        new LatLng(45.0, -12.0));
+
+        rectangulo.strokeWidth(8);
+        rectangulo.strokeColor(Color.RED);
+
+        mMap.addPolygon(rectangulo);
+    }
 }
